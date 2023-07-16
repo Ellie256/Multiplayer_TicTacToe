2b80f7101f9a1ab016e807bf68a217b78372ba05
Ellie256 <61980311+Ellie256@users.noreply.github.com>
2023-07-16 14:07:36 -0400

    Added create game functionality

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/************** Set Up *******************/
 // Send the HTML file to the client
 var express = require('express');
 var app = express();
@@ -7,33 +8,44 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client', express.static(__dirname + '/client'));
-serv.listen(2000);
+serv.listen(process.env.PORT || 2000);
 console.log('Server Started.');
 
-// Set up lists
-var SOCKET_LIST = {};
-
+/************** Objects *****************/
+/************* Player ******************/
 // Player Constructor
-var Player = function(id) {
+var Player = function(param) {
 	var self = {
-		x: 50,
-		y: 50,
-		id: id,
-		number: "" + Math.floor(10 * Math.random())
+		id: "",
+		chip: '',
+		myTurn: false
+	}
+
+	if(param) {
+		if(param.id)
+			self.id = param.id;
+		if(param.chip)
+			self.chip = param.chip;
+		if(param.myTurn)
+			self.myTurn = param.myTurn;
 	}
 
 	self.update = function() {
-		self.x++;
-		self.y++;
+		// self.x++;
+		// self.y++;
 	}
 
-	Player.list[id] = self;
+	Player.list[self.id] = self;
 	return self;
 }
 Player.list = {};
 
-Player.onConnect = function(socket) {
-	Player(socket.id);
+Player.onConnect = function(socket, chip, myTurn) {
+	Player({
+		id: socket.id,
+		chip: chip,
+		myTurn: myTurn
+	});
 }
 
 Player.onDisconnect = function(socket) {
@@ -55,23 +67,82 @@ Player.update = function() {
 	return pack;
 }
 
+/**************** Game ***************/
 // Game Constructor
-var Game = function(id) {
+var Game = function(param) {
 	var self = {
-		id: id,
-		players: [],
-		chips: []
+		id: "",
+		p1: null,
+		p2: null,
+		winner: "",
+		board_state: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+	}
+
+	if(param) {
+		if(param.id)
+			self.id = param.id;
+		if(param.p1)
+			self.p1 = param.p1;
+		if(param.p2)
+			self.p2 = param.p2;
 	}
 
 	self.update = function() {
-		self.x++;
-		self.y++;
+		// self.x++;
+		// self.y++;
 	}
 
-	Game.list[id] = self;
+	self.checkWin = function() {
+		// 0 1 2
+        // 3 4 5
+        // 6 7 8
+		row1 = self.board_state[0] + self.board_state[1] + self.board_state[2]
+        row2 = self.board_state[3] + self.board_state[4] + self.board_state[5]
+        row3 = self.board_state[6] + self.board_state[7] + self.board_state[8]
+        col1 = self.board_state[0] + self.board_state[3] + self.board_state[6]
+        col2 = self.board_state[1] + self.board_state[4] + self.board_state[7]
+        col3 = self.board_state[2] + self.board_state[5] + self.board_state[8]
+        dia1 = self.board_state[0] + self.board_state[4] + self.board_state[8]
+        dia2 = self.board_state[2] + self.board_state[4] + self.board_state[6]
+        if (row1 === 3 || row2 === 3 || row3 === 3 || col1 === 3 || col2 === 3 || col3 === 3 || dia1 === 3 || dia2 === 3)
+            self.winner = 'P1'
+        else if (row1 === -3 || row2 === -3 || row3 === -3 || col1 === -3 || col2 === -3 || col3 === -3 || dia1 === -3 || dia2 === -3)
+            self.winner = 'P2'
+        
+        counter = 0
+        for (var i in self.board_state.length) {
+			if (self.board_state[i] != 0)
+                counter += 1
+		}
+        if (counter == 9 && self.winner == '')
+            self.winner = 'Tie'
+	}
+
+	Game.list[self.id] = self;
 	return self;
 }
 Game.list = {};
+
+
+var SOCKET_LIST = {};
+
+var isGameFull = function(data) {
+	if (Game.list[data.gameId].p2 === null)
+		return false;
+	else
+		return true;
+}
+var doesGameExist = function(data) {
+	return Game.list[data.gameId];
+}
+var addGame = function(data) {
+	Game({
+		id: data.gameId,
+		p1: data.name
+	});
+}
+
+var DEBUG = true;
 
 // Set up the sockets
 var io = require('socket.io')(serv, {});
@@ -79,12 +150,49 @@ io.sockets.on('connection', function(socket) {
 	socket.id = Math.random();
 	SOCKET_LIST[socket.id] = socket;
 
-	Player.onConnect(socket);
+	socket.on('createGame', function() {
+		var gameId = "" + Math.floor(10000 * Math.random())
+		while(doesGameExist({gameId: gameId}))
+			gameId = "" + Math.floor(10000 * Math.random())
+		Player.onConnect(socket, 'x', true);
+		addGame({gameId: gameId, name:socket.id});
+		socket.emit('createGameResponse', {success:true});
+	});
+
+	socket.on('joinGame', function(data) {
+		if (!doesGameExist(data)) {
+			socket.emit('joinGameResponse', {success:false, msg: "Game ID doesn't exist"});
+		}
+		else if (isGameFull(data)) {
+			socket.emit('joinGameResponse', {success:false, msg: "Game is full"});
+		}
+		else {
+			Player.onConnect(socket, 'o', false);
+			Game.list[data.gameId].p2 = socket.id;
+			socket.emit('joinGameResponse', {success:true});
+		}
+	});
 
 	socket.on('disconnect', function() {
 		delete SOCKET_LIST[socket.id];
 		Player.onDisconnect(socket);
 	});
+
+	socket.on('sendMsgToServer', function(data) {
+		var playerName = ("" + socket.id).slice(2, 7);
+		for(var i in SOCKET_LIST) {
+			SOCKET_LIST[i].emit('addToChat', playerName + ": " + data);
+		}
+	});
+
+	socket.on('evalServer', function(data) {
+		if(!DEBUG)
+			return;
+		
+		var res = eval(data);
+		socket.emit('evalAnswer', res);
+	});
+
 });
 
 
--- client/js/index.js
+++ client/js/index.js
@@ -1,36 +1,151 @@
-// Set up Canvas
-var ctx = document.getElementById("ctx").getContext("2d");
-ctx.font = '30px Arial';
-
 // Connect to the socket.io server
 var socket = io();
 
-socket.on('newPositions', function(data) {
-	ctx.clearRect(0, 0, 500, 500);
-	for (var i = 0; i < data.length; i++) {
-		ctx.fillText(data[i].number, data[i].x, data[i].y);
-	}
-	drawBoard();
-});
+/***************** Game Code ***********************/
+// Set up Canvas
+var ctxBoard = document.getElementById("ctx-board").getContext("2d");
+var ctx = document.getElementById("ctx").getContext("2d");
+ctx.font = '30px Arial';
+drawBoard();
+
+// socket.on('newPositions', function(data) {
+// 	ctx.clearRect(0, 0, 500, 500);
+// 	for (var i = 0; i < data.length; i++) {
+// 		ctx.fillText(data[i].number, data[i].x, data[i].y);
+// 	}
+// });
 
 function drawBoard() {
 	// Line 1
-	ctx.moveTo(175, 50);
-	ctx.lineTo(175, 450);
-	ctx.stroke();
+	ctxBoard.moveTo(175, 50);
+	ctxBoard.lineTo(175, 450);
+	ctxBoard.stroke();
 
 	// Line 2
-	ctx.moveTo(325, 50);
-	ctx.lineTo(325, 450);
-	ctx.stroke();
+	ctxBoard.moveTo(325, 50);
+	ctxBoard.lineTo(325, 450);
+	ctxBoard.stroke();
 
 	// Line 3
-	ctx.moveTo(50, 175);
-	ctx.lineTo(450, 175);
-	ctx.stroke();
+	ctxBoard.moveTo(50, 175);
+	ctxBoard.lineTo(450, 175);
+	ctxBoard.stroke();
 
 	// Line 4
-	ctx.moveTo(50, 325);
-	ctx.lineTo(450, 325);
-	ctx.stroke();
+	ctxBoard.moveTo(50, 325);
+	ctxBoard.lineTo(450, 325);
+	ctxBoard.stroke();
+}
+
+// Get mouse events
+document.onmousedown = function(event) {
+	var x = event.clientX;
+	var y = event.clientY;
+
+	if (x > 50 && x < 450 && y > 50 && y < 450) {
+		// Box 1
+		if (x < 175 && y < 175) {
+			console.log(1)
+		}
+		// Box 2
+		else if (x > 175 && x < 325 && y < 175) {
+			console.log(2)
+		}
+		// Box 3
+		else if (x > 325 && y < 175) {
+			console.log(3)
+		}
+		// Box 4
+		else if (x < 175 && y > 175 && y < 325) {
+			console.log(4)
+		}
+		// Box 5
+		else if (x > 175 && x < 325 && y > 175 && y < 325) {
+			console.log(5)
+		}
+		// Box 6
+		else if (x > 325 && y > 175 && y < 325) {
+			console.log(6)
+		}
+		// Box 7
+		else if (x < 175 && y > 325) {
+			console.log(7)
+		}
+		// Box 8
+		else if (x > 175 && x < 325 && y > 325) {
+			console.log(8)
+		}
+		// Box 9
+		else {
+			console.log(9)
+		}
+	}
+	
+	// socket.emit('keyPress', {inputId: 'attack', state:true});
+}
+
+/**************** Sign In Code *******************/
+var joinDiv = document.getElementById('joinDiv');
+var joinBtn = document.getElementById('join-btn');
+var createBtn = document.getElementById('create-btn');
+var joinInput = document.getElementById('join-input');
+var gameDiv = document.getElementById('gameDiv');
+
+// Join Game
+joinBtn.onclick = function() {
+	socket.emit('joinGame', {gameId: joinInput.value});
+}
+socket.on('joinGameResponse', function(data) {
+	if(data.success) {
+		joinDiv.style.display = 'none';
+		gameDiv.style.display = 'inline-block';
+	}
+	else {
+		if (data.msg)
+			alert(data.msg);
+		else
+			alert("Join Game Unsuccessful");
+	}
+});
+
+// Create Game
+createBtn.onclick = function() {
+	socket.emit('createGame');
+}
+socket.on('createGameResponse', function(data) {
+	if(data.success) {
+		joinDiv.style.display = 'none';
+		gameDiv.style.display = 'inline-block';
+	}
+	else {
+		alert("Create Game Unsuccessful");
+	}
+});
+
+
+/***************** Chat Box *******************/
+// Get Chat box
+var chatText = document.getElementById('chat-text');
+var chatInput = document.getElementById('chat-in');
+var chatForm = document.getElementById('chat-form');
+
+// Add chat text
+socket.on('addToChat', function(data) {
+	chatText.innerHTML += '<div>' + data + '</div>';
+});
+
+// Add get debug answer
+socket.on('evalAnswer', function(data) {
+	console.log(data);
+});
+
+chatForm.onsubmit = function(e) {
+	// Must call prevent default to prevent a page refresh
+	e.preventDefault();
+
+	if(chatInput.value[0] === '/')
+		socket.emit('evalServer', chatInput.value.slice(1))
+	else
+		socket.emit('sendMsgToServer', chatInput.value);
+	chatInput.value = '';
 }
